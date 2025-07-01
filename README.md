c5007175efa608e44c835b928ea691478a60dbe8
Pete Millspaugh <peterdgmillspaugh@gmail.com>
2025-07-01 16:31:16 -0400

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotcom.press
+
+Dot Com Press is a book publisher designed for the Internet age, and [dotcom.press](https://dotcom.press) is its home.
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <>
+      <header>
+        <img src="./icon.svg" alt="Dot Com Press" />
+      </header>
+      <main>
+        <h1>Publishing for the Internet age.</h1>
+      </main>
+      <footer>© 2025 Dot Com Press, LLC</footer>
+    </>
+  );
+}
