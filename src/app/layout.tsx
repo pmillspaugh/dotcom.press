1de009e2d845406f7089ae501fbc45eabeefb6ec
Pete Millspaugh <peterdgmillspaugh@gmail.com>
2025-07-01 20:23:27 -0400

    Use Commit Mono for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
+import CommitMono from "next/font/local";
 import "./globals.css";
+
+const commitMono = CommitMono({
+  src: "./CommitMono.woff2",
+  variable: "--commit-mono",
+});
 
 export const metadata: Metadata = {
   title: "Dot Com Press",
@@ -12,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={commitMono.className}>
       <body>{children}</body>
     </html>
   );
