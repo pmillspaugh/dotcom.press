00d9d703b8644ba0d7c5ccf995dc7fd8dc64f225
Pete Millspaugh <peterdgmillspaugh@gmail.com>
2026-04-06 16:43:32 -0400

    Rewrite blurb

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,37 +9,45 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.h1}>
           <span>dot com et al.</span>
-          <span>The secret life of domains</span>
+          <span>The secret life of Internet domains</span>
         </h1>
-        <p className={styles.separator}>...</p>
-        <p>
-          OpenAI made headlines in 2023 when it bought <em>chat.com</em> for
-          $15m to host ChatGPT, but big ticket domain purchases are nothing new:{" "}
-          <em>hotels.com</em> went for $11m in 2001, and <em>sex.com</em> sold
-          for $12m in 2006.
+        <p className={styles.separator}>***</p>
+        <p className={styles.p}>
+          The story of domain names might seem narrow—as narrow as your
+          browser's address bar. But there's a whole world hidden in those
+          little URLs. The life of internet domains is one of geopolitical
+          drama, thorny ethics and governance, technical marvel, artistic
+          expression, and economic power.
         </p>
-        <p>
-          Anguilla, the small island nation, earns over 20% of its revenue on
-          sales of <code className={styles.code}>.ai</code> domain names. When
-          Yugoslavia split in the 90s, a group of Slovenian scientists conducted
-          a literal heist to steal domain records for the{" "}
-          <code className={styles.code}>.yu</code> top-level domain (TLD).
+        <p className={styles.p}>
+          Like how the small island nation of Anguilla earns nearly half its
+          national revenue selling <code className={styles.code}>.ai</code>{" "}
+          domains (<Link href="/archive/anguilla">1</Link>
+          ). Or how in the 90s when Yugoslavia split, a group of Slovenian
+          scientists broke into an IT building to steal{" "}
+          <code className={styles.code}>.yu</code> domain records and literally
+          cut the building's internet access with scissors (
+          <Link href="/archive/eminent-domains">2</Link>). Or that an early
+          domain investor registered <em>sex.com</em> for free and later sold it
+          for $13 million.
         </p>
-        <p>
-          Ten years ago, Paul Graham argued in his essay{" "}
-          <em>Change Your Name</em> that all serious companies must own a{" "}
-          <code className={styles.code}>.com</code> domain. Does that still hold
-          true today? There are now hundreds of TLDs, like{" "}
+        <p className={styles.p}>
+          There are now over a thousand top-level domains—the part to the right
+          of the dot, like <code className={styles.code}>.net</code> and 
           <code className={styles.code}>.nyc</code>,{" "}
-          <code className={styles.code}>.band</code>,{" "}
-          <code className={styles.code}>.pizza</code>, and even{" "}
-          <code className={styles.code}>.wtf</code>. Who creates these? And how
-          do they decide how much you pay?
+          <code className={styles.code}>.porn</code> and{" "}
+          <code className={styles.code}>.pizza</code>, or{" "}
+          <code className={styles.code}>.gay</code> and{" "}
+          <code className={styles.code}>.google</code>. Who creates these? How
+          do they decide how much you pay? And who makes the rules?
         </p>
-        <p>
-          <em>dot com et al.</em> explores the history, economics, and artistry
-          of domains. Sign up to <Link href="/archive">read emails</Link> (it'll
-          help you decide whether to read the book).
+        <p className={styles.p}>
+          Last year, <Link href="/history-of-domains">dot com turned 40</Link>.
+          Four decades and a billion registered domains later,{" "}
+          <em>dot com et al.</em> will explore the history, economics,
+          geopolitics, artistry, and governance of domains. Subscribe to the
+          book's <Link href="/archive">email newsletter</Link> ahead of its
+          publication:
         </p>
         <Signup />
       </main>
